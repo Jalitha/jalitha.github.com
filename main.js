c98d1334a57321392f90bc70f9c781f2765aa7f5
Jalitha <jalitha@identitii.com>
2021-03-29 23:25:16 +1100

    basic framework

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el:'#app',
+    data: {
+        product: 'Socks',
+        inventory: 0,
+        description: 'they good.',
+        link: 'https://www.gofundme.com/f/vuske6-feed-a-thousand-lives?ref=jalitha',    
+    }
+})
