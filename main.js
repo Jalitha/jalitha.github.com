cdba3819ea7a7a335aa12481e50645e1ead1f736
Jalitha <jdes_10@hotmail.com>
2021-10-20 14:55:13 +1100

    start

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+starts = [
+    "Champ, ",
+"Fact: ",
+"Everybody says ",
+"Dang… ",
+"Check it: ",
+"Just saying… ",
+"Superstar, ",
+"Tiger, ",
+"Self, ",
+"Know this: ",
+"News alert: ",
+"Girl, ",
+"Ace, ",
+"Excuse me but ",
+"Experts agree: ",
+"In my opinion, ",
+"Hear ye, hear ye: ",
+"Okay, listen up: "
+]
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+start = starts[getRandomInt(18)]
+
 $('#peptalk').text('Done')
