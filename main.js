daa7dabf26dc4151712a082243d11915c81e5bab
Jalitha <jdes_10@hotmail.com>
2021-10-20 14:46:28 +1100

    main test

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-var app = new Vue({
-    el:'#app',
-    data: {
-        product: 'Socks',
-        inventory: 0,
-        description: 'they good.',
-        link: 'https://www.gofundme.com/f/vuske6-feed-a-thousand-lives?ref=jalitha',    
-    }
-})
+$('#peptalk').text('Done')
