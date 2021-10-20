edc77713afdb7de46ac46c551ae0462993e3366f
Jalitha <jdes_10@hotmail.com>
2021-10-20 15:32:18 +1100

    middle

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 starts = [
-    "Champ, ",
+"Champ, ",
 "Fact: ",
 "Everybody says ",
 "Dang… ",
@@ -19,10 +19,34 @@ starts = [
 "Okay, listen up: "
 ]
 
+
+middles = [
+"the mere idea of you ",
+"your soul ",
+"your hair today ",
+"everything you do ",
+"your personal style ",
+"every thought you have ",
+"that sparkle in your eye ",
+"your presence here ",
+"what you got going on ",
+"the essential you ",
+"your life's journey ",
+"that saucy personality ",
+"your DNA ",
+"that brain of yours ",
+"your choice of attire ",
+"the way you roll ",
+"whatever your secret is ",
+"all of y'all ",
+]
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
 start = starts[getRandomInt(18)]
+middle = middles[getRandomInt(18)]
 
-$('#peptalk').text(start)
+sentence = start + middle 
+$('#peptalk').text(sentence)
